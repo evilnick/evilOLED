160dbc28b0eddb3b5b75545f9e9abdb0ac155efe
evilnick <nick.veitch@canonical.com>
2014-10-26 20:44:12 +0000

    Update evilOLED.h

--- evilOLED.h
+++ evilOLED.h
@@ -1,3 +1,12 @@
+/*
+evilOLED is an Arduino library for efficient use of SSD1306 based 
+displays, specifically 128x64 pixel ones using the I2C 2-wire
+interface. This driver does not use a framebuffer, therefore
+leaving you with plenty of dynamic memory for your own code!
+Written by Nick Veitch, 2014 <veryevilnick@gmail.com>
+Released under MIT license
+Latest versions available at https://github.com/evilnick/evilOLED/
+*/
 #ifndef _EVILOLED_
 #define _EVILOLED_
 
@@ -24,19 +33,19 @@ using namespace std;
 class evilOLED {
  public:
   evilOLED(char sda, char scl);
-  void dataStart();
-  void dataStop();
-  void sendByte(unsigned char data);
-  void sendCmd(unsigned char cmd);
-  void init();
-  void cls(char);
-  void setCursor(char x, char y);
-  void putChar(char);
-  void putString(char*);
-  void putString(int s);
-  void splash();
-  void flash(int d);
-  void alert(int d=200);
+  void dataStart();                  //initiate data transfer mode (low level)
+  void dataStop();                   //finish data transfer mode (low level)
+  void sendByte(unsigned char data); //send a single byte to device (low level)
+  void sendCmd(unsigned char cmd);   //send a single command (see datasheet)
+  void init();                       //initialise display
+  void cls(char);                    //clear the display (optionally with specific byte value)
+  void setCursor(char x, char y);    //set the text cursor position (0-15,0-7)
+  void putChar(char);                //write a single character to the display
+  void putString(char*);             //write a string to the display
+  void putString(int s);             //write a number to the display
+  void splash();                     //display the splash screen
+  void flash(int d);                 //flash the display (with specified delay)
+  void alert(int d=200);             //rapid flash the screen to alert user (with optional delay)
  private:
   char _sda;
   char _scl;
